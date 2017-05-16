2be803e98024cc7c4df6114157b34f1d6a630cbd
Silence <374619540@qq.com>
2017-05-16 12:48:12 +0800

    Update

---
+++ MetaManager/MediaMetaManager.h
@@ -0,0 +1,16 @@
+//
+//  MediaMetaManager.h
+//  MetaManagerDemo
+//
+//  Created by 杨晴贺 on 2017/5/16.
+//  Copyright © 2017年 Silence. All rights reserved.
+//
+
+#ifndef MediaMetaManager_h
+#define MediaMetaManager_h
+
+#import "SIMediaItem.h"
+#import "SIMetadata.h"
+#import "SIGenre.h"
+
+#endif /* MediaMetaManager_h */
